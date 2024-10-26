f4d5b136b367da23c1c06af9058df883041b7281
Adham <adhamusama25@gmail.com>
2024-10-27 01:16:07 +0300

    delete task

--- src/components/SingleTaskViewer/SingleTaskViewer.tsx
+++ src/components/SingleTaskViewer/SingleTaskViewer.tsx
@@ -2,17 +2,56 @@ import React, { useEffect } from "react";
 import CustomDialog from "../UI/CustomDialog/CustomDialog";
 import { ITask } from "../../types/tasks.types";
 import classes from "./SingleTaskViewer.module.css";
+import { useAppDispatch } from "../../redux/hooks";
+import { deleteTask } from "../../redux/features/tasks/tasksSlice";
+
+const statusToTextMap = {
+  todo: "To Do",
+  doing: "Doing",
+  done: "Done",
+};
+const priorityToTextMap = {
+  low: "Low Priority",
+  medium: "Medium Priority",
+  high: "High Priority",
+};
+
+const statusToStyleMap = {
+  todo: {
+    backgroundColor: "var(--to-do-color)",
+  },
+  doing: {
+    backgroundColor: "var(--doing-color)",
+  },
+  done: {
+    backgroundColor: "var(--done-color)",
+  },
+};
+
+const priorityToStyleMap = {
+  low: {
+    backgroundColor: "var(--low-priority-color)",
+  },
+  medium: {
+    backgroundColor: "var(--medium-priority-color)",
+  },
+  high: {
+    backgroundColor: "var(--high-priority-color)",
+  },
+};
 
 const SingleTaskViewer = ({
   task,
   isOpen,
-  setIsOpen
+  setIsOpen,
 }: {
   task: ITask;
   isOpen: boolean;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const dispatch = useAppDispatch();
   const dialogRef = React.useRef<HTMLDialogElement>(null);
+  const deleteDialogRef = React.useRef<HTMLDialogElement>(null);
 
   useEffect(() => {
     if (dialogRef.current) {
@@ -25,14 +64,52 @@ const SingleTaskViewer = ({
   }, [isOpen]);
 
   return (
-    <CustomDialog dialogRef={dialogRef} preCloseDialogAction={()=>setIsOpen(false)}>
-      <div className={classes.SingleTaskViewer}>
+    <>
+      <CustomDialog showCloseButton={false} dialogRef={deleteDialogRef}>
+        <div className={classes.DeleteTaskPopUp}>
+          <p>Are you sure you want to delete this task?</p>
+          <div className={classes.Actions}>
 
-      <img src={task.image} alt={task.title}/>
-      <h2>{task.title}</h2>
-      <p>{task.description}</p>
-      </div>
-    </CustomDialog>
+          <button onClick={() => dispatch(deleteTask(task))}>Yes</button>
+          <button className={classes.CancelBtn} onClick={() => deleteDialogRef.current?.close()}>Cancel</button>
+          </div>
+        </div>
+      </CustomDialog>
+      <CustomDialog
+        dialogRef={dialogRef}
+        preCloseDialogAction={() => setIsOpen(false)}
+      >
+        <div className={classes.SingleTaskViewer}>
+          {task.image && <img src={task.image} alt={task.title} />}
+
+          <div className={classes.Content}>
+            <h2>{task.title}</h2>
+            <p>{task.description}</p>
+
+            <div className={classes.StatusAndPriority}>
+              <p style={statusToStyleMap[task.state]}>
+                {statusToTextMap[task.state]}
+              </p>
+              <p
+                style={{ ...priorityToStyleMap[task.priority], color: "white" }}
+              >
+                {priorityToTextMap[task.priority]}
+              </p>
+            </div>
+
+            <div className={classes.Actions}>
+              <button onClick={() => null}>Edit</button>
+              <button
+                onClick={() => deleteDialogRef.current?.showModal()}
+                className={classes.DeleteBtn}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </CustomDialog>
+    </>
   );
 };
 
--- src/components/UI/CustomDialog/CustomDialog.tsx
+++ src/components/UI/CustomDialog/CustomDialog.tsx
@@ -3,11 +3,13 @@ import classes from "./CustomDialog.module.css";
 interface CustomDialogProps {
   children: React.ReactNode;
   dialogRef: React.RefObject<HTMLDialogElement>;
+  showCloseButton?: boolean;
   preCloseDialogAction?: () => void;
 }
 const CustomDialog = ({
   children,
   dialogRef,
+  showCloseButton = true,
   preCloseDialogAction,
 }: CustomDialogProps) => {
 
@@ -19,7 +21,7 @@ const CustomDialog = ({
   return (
     <dialog onClose={preCloseDialogAction} ref={dialogRef} className={classes.CustomDialog}>
       {children}
-      <button className={ classes.CloseButton}onClick={closeDialog}>×</button>
+     {showCloseButton && <button className={ classes.CloseButton}onClick={closeDialog}>×</button>}
     </dialog>
   );
 };
